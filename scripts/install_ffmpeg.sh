e1af9e28e67db57a3df3c37ca40096727e4f69d3
Xiangliang Meng <meng.xiangliang1985@gmail.com>
2018-11-20 00:32:00 +0900

    Update install_ffmpeg.sh

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -4,6 +4,4 @@ cd /usr/local/src
 curl http://ffmpeg.org/releases/ffmpeg-4.1.tar.bz2 | tar -xj
 
 cd ffmpeg-4.1
-./configure
-make
-make install
+nice -n 19 ./configure && nice -n 19 make && nice -n 19 make install
